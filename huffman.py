8edd42852b609648e568d048ae2cd7cb6de8b98f
Michael Yagodarov <purposelessness@yandex.ru>
2022-12-23 18:36:57 +0300

    feat: step-by-step logging

--- huffman.py
+++ huffman.py
@@ -8,17 +8,26 @@ class Node(namedtuple("Node", ["left", "right"])):
         self.left.walk(code, acc + "0")
         self.right.walk(code, acc + "1")
 
+    def __str__(self):
+        return f"[left = {self.left} | right = {self.right}]"
+
 
 class Leaf(namedtuple("Leaf", ["char"])):
     def walk(self, code, acc):
         code[self.char] = acc or "0"
+        print(f"'{self.char}' = {acc}")
+
+    def __str__(self):
+        return f"('{self.char}')"
 
 
 def huffman_encode(s: str):
     h = []
+    print(f"Encoding string: {s}")
     for ch, freq in Counter(s).items():
         h.append((freq, len(h), Leaf(ch)))
     heapq.heapify(h)
+    print(f"Heap: {[f'{freq}, {str(val)}' for (freq, count, val) in h]}")
     count = len(h)
     while len(h) > 1:
         freq_1, count_1, left = heapq.heappop(h)
@@ -26,6 +35,7 @@ def huffman_encode(s: str):
 
         heapq.heappush(h, (freq_1 + freq_2, count, Node(left, right)))
 
+        print(f"Heap: {[f'{freq}, {str(val)}' for (freq, count, val) in h]}")
         count += 1
     code = {}
 
@@ -37,12 +47,14 @@ def huffman_encode(s: str):
 
 def huffman_decode(encoded: str, code):
     res = []
+    print(f"Decoding string: {encoded}")
     encoded_ch = ""
     for ch in encoded:
         encoded_ch += ch
         for decoded_ch in code:
             if code.get(decoded_ch) == encoded_ch:
                 res.append(decoded_ch)
+                print(f"{encoded_ch} = {decoded_ch} -> result: {''.join(res)}")
                 encoded_ch = ""
                 break
     return "".join(res)
